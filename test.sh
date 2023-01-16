6a774028a6e81b55448b19fcfceddc2a52d218e1
Colin Hutchinson <chutchic@gmail.com>
2023-01-16 15:20:49 -0500

    fix(atc-router): fix how atc router gets included

--- test.sh
+++ test.sh
@@ -14,6 +14,7 @@ function test() {
     cp -R /tmp/build/* /
     mv /tmp/build /tmp/buffer # Check we didn't link dependencies to `/tmp/build/...`
 
+    ls -la /tmp/buffer/usr/local/openresty/bin/openresty
     /usr/local/openresty/bin/openresty -v 2>&1 | grep -q ${OPENRESTY_VERSION}
     /usr/local/openresty/bin/openresty -V 2>&1 | grep -q pcre
     /usr/local/openresty/bin/openresty -V 2>&1 | grep -q with-pcre=
@@ -22,12 +23,15 @@ function test() {
     /usr/local/openresty/bin/openresty -V 2>&1 | grep -q lua-resty-lmdb
     /usr/local/openresty/bin/openresty -V 2>&1 | grep -q lua-resty-events
     /usr/local/openresty/bin/resty -e 'print(jit.version)' | grep -q 'LuaJIT[[:space:]][[:digit:]]\+.[[:digit:]]\+.[[:digit:]]\+-[[:digit:]]\{8\}'
+    ls -la /tmp/buffer/usr/local/openresty/nginx/sbin/nginx
+    ls -la /tmp/buffer/usr/local/openresty/bin/resty
 
     ls -l /usr/local/openresty/lualib/resty/websocket/*.lua
     grep _VERSION /usr/local/openresty/lualib/resty/websocket/*.lua
     luarocks --version
 
-    ls -la /usr/local/openresty/lualib/libatc_router.so
+    ls -la /tmp/buffer/usr/local/openresty/lualib/libatc_router.so
+    ls -la /tmp/buffer/usr/local/openresty/lualib/resty/router/router.lua
     #ldd /usr/local/openresty/lualib/libatc_router.so
 
     mv /tmp/buffer /tmp/build
